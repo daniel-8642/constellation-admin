c355875cacafe5699ec4a9717dd2e59117bbcb25
王海涛 <859926750@qq.com>
2021-12-09 18:28:03 +0800

    二次封装request.js 使用vue-jsx 配置环境变量

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -6,8 +6,7 @@
 
 <script>
 import Chart from "@/components/Chart";
-import axios from "axios";
-
+import request from "@/utils/request";
 export default {
   name: "Analysis",
   components: { Chart },
@@ -20,27 +19,31 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { ID: 123456 } })
-        .then((response) => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例",
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: {
+          ID: 12345,
+        },
+      }).then((response) => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例",
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data,
             },
-            tooltip: {},
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data,
-              },
-            ],
-          };
-        });
+          ],
+        };
+      });
     },
   },
   beforeDestroy() {
