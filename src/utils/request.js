6b50dadb28dbe51bbac49a15eb1888b92bf260b5
王海涛 <859926750@qq.com>
2022-01-08 00:09:46 +0800

    错误提示优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { notification } from "ant-design-vue";
 async function request(options) {
-  options.url = "http://xzys.86428642.xyz" + options.url;
+  // options.url = "http://xzys.86428642.xyz" + options.url;
+  // options.url = "http://localhost:80" + options.url;
   return axios(options)
     .then((res) => {
       return res;
     })
     .catch((error) => {
       const {
-        response: { status, statusText },
+        response: { status, statusText, data },
       } = error;
       notification.error({
         // eslint-disable-next-line no-unused-vars
         message: (h) => (
           <div>
-            请求错误 <span style="color: red">{status}</span>: {options.url}
+            请求错误 <span style="color: red">{status}</span>: {data.message}
           </div>
         ),
         description: statusText,
